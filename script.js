baa6e515c7074d0ae61fd3a8ed20534f0c0b8dcc
OstwaldAnna <asyakyna@gmail.com>
2024-03-12 10:15:06 +0300

    комментарии

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const taskInput = document.getElementById("taskInput");
-    const addTaskBtn = document.getElementById("addTaskBtn");
-    const taskList = document.getElementById("taskList");
+    const taskInput = document.getElementById("taskInput"); // Получает ссылку на элемент ввода, id "taskInput", который представляет поле для ввода новых задач.
+    const addTaskBtn = document.getElementById("addTaskBtn"); // Получает ссылку на кнопку id "addTaskBtn", которая представляет кнопку добавления новой задачи.
+    const taskList = document.getElementById("taskList"); // Получает ссылку на элемент списка id "taskList", в который будут добавляться новые задачи.
 
+    //Добавляет обработчик события "click" для кнопки добавления задачи. Когда кнопка нажата, выполняется функция, которая добавляет новую задачу в список.
     addTaskBtn.addEventListener("click", function() {
         const taskText = taskInput.value.trim();
         if (taskText !== "") {
@@ -11,11 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function addTask(taskText) {
-        const li = document.createElement("li");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.addEventListener("change", function() {
+    // Это функция, которая добавляет новую задачу в список дел.
+    function addTask(taskText) { // Это функция, которая добавляет новую задачу в список дел.
+        const li = document.createElement("li"); // Создается новый элемент списка <li>, который будет содержать задачу.
+        const checkbox = document.createElement("input"); // Создается элемент <input>, который представляет собой чекбокс для отметки задачи как выполненной.
+        checkbox.type = "checkbox"; 
+        checkbox.addEventListener("change", function() { // Добавляется обработчик события "change" к чекбоксу. При изменении состояния чекбокса, задача помечается как выполненная или не выполненная.
             if (this.checked) {
                 li.classList.add("completed");
             } else {
@@ -25,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const label = document.createElement("label");
         label.textContent = taskText;
         const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete";
-        deleteBtn.classList.add("delete-btn");
+        deleteBtn.textContent = "Delete"; 
+        deleteBtn.classList.add("delete-btn");// При нажатии на кнопку, соответствующая задача удаляется из списка.
         deleteBtn.addEventListener("click", function() {
             li.remove();
         });
